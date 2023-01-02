ddc3b01d85aa39053f406d1735a5bebb78e34428
yazanismail1 <yalfarra@outlook.com>
2023-01-02 20:00:10 +0000

    feat: added elephantSQL

--- users_manager_project/settings.py
+++ users_manager_project/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'users_manager_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": 'ecxjrvig',
+        "USER": 'ecxjrvig',
+        "PASSWORD": 'Aq8bV1OT3vJSGFQuyr3uB7HkLnq2xCNZ',
+        "HOST": 'snuffleupagus.db.elephantsql.com',
+        "PORT": 5432,
     }
 }
 
